73f0321c9c79bc0b7211f8fc9b6e94df3151d230
guo <dingfr2@oocl.com>
2015-08-25 14:35:43 +0800

    RestType和Rest双向连接

--- O2OSystem-customer/src/com/g04/o2o/dao/impl/RestaurantDaoImpl.java
+++ O2OSystem-customer/src/com/g04/o2o/dao/impl/RestaurantDaoImpl.java
@@ -2,6 +2,8 @@ package com.g04.o2o.dao.impl;
 
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.g04.o2o.dao.RestaurantDao;
--- O2OSystem-customer/src/com/g04/o2o/entity/MenuType.java
+++ O2OSystem-customer/src/com/g04/o2o/entity/MenuType.java
@@ -1,9 +1,15 @@
 package com.g04.o2o.entity;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 /**
@@ -17,7 +23,21 @@ public class MenuType {
 	private Integer id;
 	private String MenuTypeName;
 	private String creater;
+	@OneToMany(mappedBy="type")
+	private Set<Restaurant> hasRestaurants = new HashSet<Restaurant>();
 	
+	/**
+	 * @return the hasRestaurants
+	 */
+	public Set<Restaurant> getHasRestaurants() {
+		return hasRestaurants;
+	}
+	/**
+	 * @param hasRestaurants the hasRestaurants to set
+	 */
+	public void setHasRestaurants(Set<Restaurant> hasRestaurants) {
+		this.hasRestaurants = hasRestaurants;
+	}
 	public Integer getId() {
 		return id;
 	}
