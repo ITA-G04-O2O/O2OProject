538b032501a561191bca29c9a9c86c2b40f22299
ouok <okar.ou@oocl.com>
2015-08-25 15:34:07 +0800

    update menuType Service

--- O2OSystem-merchant/src/com/g04/o2o/service/MenuService.java
+++ O2OSystem-merchant/src/com/g04/o2o/service/MenuService.java
@@ -1,5 +1,19 @@
 package com.g04.o2o.service;
 
-public interface MenuService {
+import java.util.List;
 
+public interface MenuService {
+	//menuType
+	
+	/**
+	 * 获取所有菜单种类
+	 * @return
+	 */
+	List<String> findAllMenuType();
+	boolean addMenuType(String menuType);
+	boolean delMenuType(String menuType);
+	boolean updMenuType(Integer id, String menuType);
+	
+	//menuItems
+	
 }
