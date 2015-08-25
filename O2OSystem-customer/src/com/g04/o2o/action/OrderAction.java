615a7012e093484b7fcad00d3a8cf1263dbac418
CHENJA9 <CHENJA9@ZHA-ITA016-W7.corp.oocl.com>
2015-08-25 20:52:53 +0800

    update action

--- O2OSystem-customer/src/com/g04/o2o/action/OrderAction.java
+++ O2OSystem-customer/src/com/g04/o2o/action/OrderAction.java
@@ -1,28 +1,47 @@
 package com.g04.o2o.action;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.g04.o2o.entity.JsonProtocol;
 import com.g04.o2o.entity.Order;
 import com.g04.o2o.service.OrderService;
 
 @RequestMapping()
 @RestController
 public class OrderAction {
-
+	@Autowired
 	private OrderService orderService;
 	
 	@RequestMapping(value="/orders",method=RequestMethod.POST)
-	public int addOrder(Order o){
-		int result=orderService.addOrder(o);
-		return result;
+	public JsonProtocol addOrder(Order o){
+		JsonProtocol jp = new JsonProtocol();
+		jp.setObject(orderService.addOrder(o));
+		return jp;
 	}
 	
 	@RequestMapping(value="/orders/{id}",method=RequestMethod.GET)
-	public Order getOrder(@PathVariable(value="id") Integer oid){
-		return orderService.getOrder(oid);
+	public JsonProtocol getOrder(@PathVariable(value="id") Integer oid){
+		JsonProtocol jp = new JsonProtocol();
+		jp.setObject(orderService.getOrder(oid));
+		return jp;
+	}
+	
+	@RequestMapping(value="/orders",method=RequestMethod.POST)
+	public JsonProtocol updateMessage(Integer oId,String notice){
+		JsonProtocol jp = new JsonProtocol();
+		jp.setObject(orderService.updateMessage(oId, notice));
+		return jp;
+	}
+	
+	@RequestMapping(value="/orders",method=RequestMethod.POST)
+	public JsonProtocol updateScore(Integer oId,Integer sc){
+		JsonProtocol jp = new JsonProtocol();
+		jp.setObject(orderService.updateScore(oId, sc));
+		return jp;
 	}
 	
 }
