cbd14842fb33c4c7215e35f6864591f9277b7735
LILE8 <380034641@qq.com>
2015-08-25 16:57:07 +0800

    leo add merchant service

---
+++ O2OSystem-merchant/src/com/g04/o2o/service/MerchantService.java
@@ -0,0 +1,29 @@
+package com.g04.o2o.service;
+
+import java.util.List;
+
+import com.g04.o2o.entity.Merchant;
+
+/**
+ * 
+ * 商家服务
+ * 
+ * @author LILE8
+ * 
+ */
+public interface MerchantService {
+	/**
+	 * 新增商家
+	 * 
+	 * @param merchant
+	 * @return
+	 */
+	public int addMerchant(Merchant merchant);
+	
+	/**
+	 * 找到所有商家
+	 * @return
+	 */
+	public List<Merchant> findAllMerchant();
+	
+}
