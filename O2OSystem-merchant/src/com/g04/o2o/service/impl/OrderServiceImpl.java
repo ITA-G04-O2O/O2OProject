9e27acc1b7e3d969ab0ebcda896203bf849611b6
ouok <okar.ou@oocl.com>
2015-08-25 21:28:28 +0800

    update OrderServiceImpl

--- O2OSystem-merchant/src/com/g04/o2o/service/impl/OrderServiceImpl.java
+++ O2OSystem-merchant/src/com/g04/o2o/service/impl/OrderServiceImpl.java
@@ -3,16 +3,13 @@ package com.g04.o2o.service.impl;
 import java.util.Date;
 import java.util.List;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.g04.o2o.dao.OrderDao;
-=======
 import org.springframework.stereotype.Service;
 
->>>>>>> f9718c87fab240cd26340eda0a279053a2446c13
+import com.g04.o2o.dao.OrderDao;
 import com.g04.o2o.entity.Order;
 import com.g04.o2o.service.OrderService;
+
 @Service
 public class OrderServiceImpl implements OrderService {
 	@Autowired
