f6432e6229e3197b53a632dc38d763f36b91a570
ouok <okar.ou@oocl.com>
2015-08-25 17:01:53 +0800

    update OrderService in MerchantPorject

--- O2OSystem-merchant/src/com/g04/o2o/service/OrderService.java
+++ O2OSystem-merchant/src/com/g04/o2o/service/OrderService.java
@@ -11,8 +11,36 @@ import com.g04.o2o.entity.Order;
  *
  */
 public interface OrderService {
+	/**
+	 * 獲取所有訂單
+	 * @return
+	 */
 	List<Order> getAllOrders();
+	/**
+	 * 根據id獲取訂單
+	 * @param id
+	 * @return
+	 */
 	Order getOrderById(Integer id);
-	boolean updOrderReceiveTime(Integer id,Date recevieTime);
-	boolean updOrderStatus(Integer id,Integer status);
+	/**
+	 * 更新訂單接單時間
+	 * @param id
+	 * @param recevieTime
+	 * @return
+	 */
+	boolean updOrderReceiveTime(Integer id, Date recevieTime);
+	/**
+	 * 更新訂單確認訂單時間
+	 * @param id
+	 * @param recevieTime
+	 * @return
+	 */
+	boolean updOrderCompleteTime(Integer id, Date completedTime);
+	/**
+	 * 更新訂單狀態
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	boolean updOrderStatus(Integer id, Integer status);
 }
