ddf7329d57d4ae49ff758eddff6acc75887b26c7
LILE8 <380034641@qq.com>
2015-08-25 19:53:24 +0800

    load

--- O2OSystem-merchant/src/com/g04/o2o/action/RestaurantAction.java
+++ O2OSystem-merchant/src/com/g04/o2o/action/RestaurantAction.java
@@ -1,11 +1,26 @@
 package com.g04.o2o.action;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.g04.o2o.service.RestaurantService;
 
-@Controller
+@RestController
 public class RestaurantAction {
+
+	@Autowired
 	private RestaurantService restaurantService;
+
+	public void setRestaurantService(RestaurantService restaurantService) {
+		this.restaurantService = restaurantService;
+	}
+
+	@RequestMapping(value = "/restaurant", method = RequestMethod.POST)
+	public String regist() {
+		
+		return "fail";
+	}
 
 }
