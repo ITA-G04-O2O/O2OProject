f9718c87fab240cd26340eda0a279053a2446c13
LILE8 <380034641@qq.com>
2015-08-25 21:15:25 +0800

    no message

--- O2OSystem-merchant/src/com/g04/o2o/action/OrderAction.java
+++ O2OSystem-merchant/src/com/g04/o2o/action/OrderAction.java
@@ -13,59 +13,29 @@ import com.g04.o2o.service.OrderService;
 
 /**
  * Order handler function
+ * 
  * @author OUOK
- *
+ * 
  */
 @RestController
 public class OrderAction {
 	@Autowired
 	private OrderService os;
-<<<<<<< HEAD
 
-	public void setOs(OrderService os) {
-		this.os = os;
-	}
-
-=======
-	
-	/**
-	 * get all orders
-	 * @return
-	 */
->>>>>>> 4094124575fd71e8a22e0eda8a9e28a44c0eccf7
 	@RequestMapping(value = "/orders", method = RequestMethod.GET)
 	public JsonProtocol getOrders() {
 		JsonProtocol jp = new JsonProtocol();
 		jp.setObject(os.getAllOrders());
 		return jp;
 	}
-<<<<<<< HEAD
 
-=======
-	
-	/**
-	 * get one order by id
-	 * @param id
-	 * @return
-	 */
->>>>>>> 4094124575fd71e8a22e0eda8a9e28a44c0eccf7
 	@RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
 	public JsonProtocol getOrder(@PathVariable Integer id) {
 		JsonProtocol jp = new JsonProtocol();
 		jp.setObject(os.getOrderById(id));
 		return jp;
 	}
-<<<<<<< HEAD
 
-=======
-	
-	/**
-	 * update order's receiveTime with id&receiveTime.
-	 * @param id
-	 * @param recevieTime
-	 * @return
-	 */
->>>>>>> 4094124575fd71e8a22e0eda8a9e28a44c0eccf7
 	@RequestMapping(value = "/orders/{id}/rece", method = RequestMethod.PUT)
 	public JsonProtocol updateOrderReceiveTime(@PathVariable Integer id,
 			Date recevieTime) {
@@ -73,17 +43,7 @@ public class OrderAction {
 		jp.setResult(os.updOrderReceiveTime(id, recevieTime));
 		return jp;
 	}
-<<<<<<< HEAD
 
-=======
-	
-	/**
-	 * update order's completedTime with id&completedTime.
-	 * @param id
-	 * @param completedTime
-	 * @return
-	 */
->>>>>>> 4094124575fd71e8a22e0eda8a9e28a44c0eccf7
 	@RequestMapping(value = "/orders/{id}/comp", method = RequestMethod.PUT)
 	public JsonProtocol updOrderCompleteTime(@PathVariable Integer id,
 			Date completedTime) {
@@ -94,6 +54,7 @@ public class OrderAction {
 
 	/**
 	 * update order's status with id&status.
+	 * 
 	 * @param id
 	 * @param status
 	 * @return
