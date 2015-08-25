4f71136a5bc0ffe5acd1727540e1fd427047dd34
ouok <okar.ou@oocl.com>
2015-08-25 19:41:24 +0800

    update MenuAction & OrderAction

---
+++ O2OSystem-merchant/src/com/g04/o2o/action/MenuAction.java
@@ -0,0 +1,10 @@
+package com.g04.o2o.action;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.g04.o2o.service.MenuService;
+
+@RestController
+public class MenuAction {
+	private MenuService ms;
+}
---
+++ O2OSystem-merchant/src/com/g04/o2o/action/OrderAction.java
@@ -0,0 +1,10 @@
+package com.g04.o2o.action;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.g04.o2o.service.OrderService;
+
+@RestController
+public class OrderAction {
+	private OrderService os;
+}
