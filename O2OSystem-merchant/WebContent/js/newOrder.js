8b1a8729a38b5a1abcde7e49b14053a22d6982ac
ouok <okar.ou@oocl.com>
2015-08-26 20:18:47 +0800

    update OrderAction & OrderVO

--- O2OSystem-merchant/WebContent/js/newOrder.js
+++ O2OSystem-merchant/WebContent/js/newOrder.js
@@ -1,4 +1,11 @@
-$(function(){
+$(function() {
 	console.log('New Order');
-	
+	$.ajax({
+		url: "http://localhost:17236/o2osystem-merchant/orders/new",
+		type:'GET',
+		dataType: 'json',
+		success: function(data) {
+			$("#jsonData").text(data.message);
+		}
+	});
 });
