65beca62cfb1553abf14cbfadb4b895a08ba4e43
guo <dingfr2@oocl.com>
2015-08-26 11:30:36 +0800

    update all pro entity...

--- O2OSystem-admin/src/com/g04/o2o/dao/RestaurantTypeDao.java
+++ O2OSystem-admin/src/com/g04/o2o/dao/RestaurantTypeDao.java
@@ -1,8 +1,13 @@
 package com.g04.o2o.dao;
 
+import java.util.Set;
+
+import com.g04.o2o.entity.Restaurant;
 import com.g04.o2o.entity.RestaurantType;
 
 
 public interface RestaurantTypeDao extends AbstractDao<RestaurantType>{
+
+	Set<Restaurant> getRestByTpe(String type);
 
 }
--- O2OSystem-admin/src/com/g04/o2o/dao/impl/RestaurantTypeDaoImpl.java
+++ O2OSystem-admin/src/com/g04/o2o/dao/impl/RestaurantTypeDaoImpl.java
@@ -1,10 +1,24 @@
 package com.g04.o2o.dao.impl;
 
+import java.util.Set;
+
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import com.g04.o2o.dao.RestaurantTypeDao;
+import com.g04.o2o.entity.Restaurant;
 import com.g04.o2o.entity.RestaurantType;
 @Repository
 public class RestaurantTypeDaoImpl extends AbstractDaoImpl<RestaurantType> implements RestaurantTypeDao{
+
+	@Override
+	public Set<Restaurant> getRestByTpe(String type) {
+		String hql="from RestaurantType where type=:type";
+		Query query=manager.createQuery(hql);
+		query.setParameter("type",type);
+		RestaurantType restType=(RestaurantType) query.getSingleResult();
+		return restType.getHasRestaurants();
+	}
 
 }
