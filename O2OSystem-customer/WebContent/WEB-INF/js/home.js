e9d9b7abad2e548f2f9676ac4a50eff2cfb54cc5
CHENJA9 <CHENJA9@ZHA-ITA016-W7.corp.oocl.com>
2015-08-26 11:33:37 +0800

    add update order status

--- O2OSystem-customer/WebContent/WEB-INF/js/home.js
+++ O2OSystem-customer/WebContent/WEB-INF/js/home.js
@@ -1,19 +1,36 @@
-var getRestType = function() {
-				$.ajax({
-					url : 'http://localhost:8888/O2OSystem-customer/',
-					type : 'get',
-					dataType : 'json'
-				}).done(function(data, status, xhr) {
-					loadData(data.books);
-				}).fail(function(xhr, status, error) {
-					console.log('fail');
-				});
-			};
-
+var restController = (function () {
+    var loadRestType = function loadRestType(data) {
+        $('div[name=restType]').empty();
+        for (var i = 0; i < data.length; i++) {
+            $('div[name=restType]').append('<div class="btn-group" role="group">
+            <button type="button" class="btn btn-default">data</button>
+        </div>');
+        }
+    };
+    var getRestType = function () {
+        $.ajax({
+            url: 'http://localhost:8888/O2OSystem-customer/restaurantType',
+            type: 'get',
+            dataType: 'json'
+        }).done(function (data, status, xhr) {
+            loadRestType(data.restTypes);
+        }).fail(function (xhr, status, error) {
+            console.log('fail');
+        });
+    };
+    return {
+        loadData: loadData,
+        addBook: addBook,
+        updateBook: updateBook,
+        getBook: getBook,
+        getBookDetail: getBookDetail,
+        deleteBook: deleteBook
+    };
+})();
 $(function () {
-	$(".restaurant").on("click", function () {
-		location.href = "restaurant.html";
-	});
-    
-    
+    $(".restaurant").on("click", function () {
+        location.href = "restaurant.html";
+    });
+
+
 });
