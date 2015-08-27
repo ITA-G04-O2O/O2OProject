0567e06b060d608a0b966005b96aaec583402482
LILE8 <380034641@qq.com>
2015-08-27 20:27:35 +0800

    no message

---
+++ O2OSystem-merchant/WebContent/js/storeInfo.js
@@ -0,0 +1,83 @@
+$(function() {
+	$("#restInfoEditBtn").live("click", function() {
+		alert($("#name").val());
+		$.ajax({
+			type : "post",
+			url : "http://localhost:17236/o2osystem-merchant/restaurant/" + 2,
+			dataType : 'json',
+			data : {
+				name : $("#name").val(),
+				type : $('#radio-group input[name="type"]:checked').val(),
+				tel : $("#tel").val(),
+				pro : $("#proBtn").text(),
+				city : $("#cityBtn").text(),
+				detail : $("#detail").val()
+			}
+
+		}).done(function(data, status, xhr) {
+			alert(data);
+		}).fail(function(xhr, status, error) {
+			console.log('fail');
+		});
+	});
+
+	$("#proBtn").live(
+			"click",
+			function() {
+				$.ajax({
+					url : 'http://localhost:17236/o2osystem-merchant/area',
+					type : 'GET',
+					dataType : 'json',
+					success : function(data) {
+						$("#province li").empty();
+						for ( var i in data) {
+							var province = data[i];
+							$('#province').append(
+									"<li id=\"" + province
+											+ "\"><a href=\"#\">" + province
+											+ "</a></li>");
+							addClickEvent(province);
+						}
+					}
+				});
+			});
+
+	function addClickEvent(province) {
+		$("#" + province).live(
+				"click",
+				function() {
+					$("#proBtn").text(province);
+					$("#city li").empty();
+					$('#cityBtn').text("城市");
+					$.ajax({
+						url : "http://localhost:17236/o2osystem-merchant/city/"
+								+ province,
+						type : 'GET',
+						dataType : 'json',
+						success : function(data) {
+							for ( var i in data) {
+								var city = data[i];
+								$('#city').append(
+										"<li id=\"" + province + city
+												+ "\"><a href=\"#\">" + city
+												+ "</a></li>");
+								$("#" + province + city)
+										.live(
+												"click",
+												function() {
+													$('#cityBtn').text(
+															$(this).text());
+													var province = $("#proBtn")
+															.text();
+													$("#provinceInput").val(
+															province);
+													$("#cityInput").val(
+															$(this).text());
+												});
+							}
+						}
+					});
+				});
+	}
+
+});
