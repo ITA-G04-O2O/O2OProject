e838f5ecb36f5a296882bc5c2c03876ae97a93b5
CHENJA9 <CHENJA9@ZHA-ITA016-W7.corp.oocl.com>
2015-08-27 16:04:59 +0800

    update

--- O2OSystem-customer/WebContent/js/restaurant.js
+++ O2OSystem-customer/WebContent/js/restaurant.js
@@ -21,27 +21,31 @@ var restInfoController = (function() {
                 
                 $('div[name=menuItems]')
                     .append(
-                            '<div class="panel panel-default"><div class="panel-heading" role="tab" id="heading'+i+'"><h4 class="panel-title"><a class="collapsed" role="button" data-toggle="collapse"data-parent="#accordion" href="menutype#'+i+'"aria-expanded="false" aria-controls="menutype'+i+'">'+data.object.menuTypes[i].menuTypeName+'</a></h4></div><div id="menutype'+i+'" class="panel-collapse collapse" role="tabpanel"aria-labelledby="heading'+i+'"><div class="panel-body"><div class="row"></div></div></div></div>');
+                            '<div class="panel panel-default"><div class="panel-heading" role="tab" id="heading'+i+'"><h4 class="panel-title"><a class="collapsed" role="button" data-toggle="collapse"data-parent="#accordion" href="menutype#'+i+'"aria-expanded="false" aria-controls="menutype'+i+'">'+data.object.menuTypes[i].menuTypeName+'</a></h4></div><div id="menutype'+i+'" class="panel-collapse collapse" role="tabpanel"aria-labelledby="heading'+i+'"><div class="panel-body"><div class="row" name="menuMealBox"></div></div></div></div>');
             
             }
-            
-            for(var j=0;j<data.object.menus.length;j++){
-                $('div[name=menuMealBox]')
-                    .append(
-                            '<div class="col-xs-4"><div class="menuMealBox" name="menuMealBox"><a href="#" class="thumbnail"><img src="img/1.jpg"></a><h4><span>'+data.object.menus[j].itemName+'</span></h4><h5 class="text-muted"><span>'+data.object.menus[j].description+'</span></h5><div class="row"><div class="col-xs-7"><h4><span>￥'+data.object.menus[j].price+'</span></h4></div><div class="col-xs-5 text-right"><button class="btn btn-primary addbtn">添加</button> </div></div></div></div>');
-            }
-            
-            
-            
-            
-            
+    
 		}).fail(function (xhr, status, error) {
 			console.log('fail');
 		});
 	};
+    
+    var getGradeInfo = function (id) {
+    	$.ajax({
+			url: 'http://localhost:8888/O2OSystem-customer/restaurant/'+id,
+			type: 'get',
+			dataType: 'json'
+		}).done(function (data, status, xhr) {
+			
+    
+		}).fail(function (xhr, status, error) {
+			console.log('fail');
+		});
+    };
 	
 	return {
-		getRestInfo : getRestInfo
+		getRestInfo : getRestInfo,
+		getGradeInfo : getGradeInfo
 	};
 	
 })();
--- O2OSystem-customer/src/vo/MenuTypeVo.java
+++ O2OSystem-customer/src/vo/MenuTypeVo.java
@@ -4,6 +4,23 @@ public class MenuTypeVo {
 
 	private Integer id;
 	private String menuTypeName;
+	public MenuTypeVo(Integer id, String menuTypeName) {
+		super();
+		this.id = id;
+		this.menuTypeName = menuTypeName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMenuTypeName() {
+		return menuTypeName;
+	}
+	public void setMenuTypeName(String menuTypeName) {
+		this.menuTypeName = menuTypeName;
+	}
 	
 	
 }
