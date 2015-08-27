3628008e98f2faacbf677e8caae3a78eed805a27
CHENJA9 <CHENJA9@ZHA-ITA016-W7.corp.oocl.com>
2015-08-27 14:57:55 +0800

    no message

--- O2OSystem-customer/src/vo/MenuTypeVo.java
+++ O2OSystem-customer/src/vo/MenuTypeVo.java
@@ -4,30 +4,6 @@ public class MenuTypeVo {
 
 	private Integer id;
 	private String menuTypeName;
-	private MenuItemVo menuIttem;
 	
-	public MenuTypeVo(Integer id, String menuTypeName, MenuItemVo menuIttem) {
-		super();
-		this.id = id;
-		this.menuTypeName = menuTypeName;
-		this.menuIttem = menuIttem;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getMenuTypeName() {
-		return menuTypeName;
-	}
-	public void setMenuTypeName(String menuTypeName) {
-		this.menuTypeName = menuTypeName;
-	}
-	public MenuItemVo getMenuIttem() {
-		return menuIttem;
-	}
-	public void setMenuIttem(MenuItemVo menuIttem) {
-		this.menuIttem = menuIttem;
-	}
+	
 }
