e2c48368c398d6a83a47176a11a6b5f10642ce17
VinceHuang <584253328@qq.com>
2015-08-28 02:03:03 +0800

    no message

--- O2OSystem-merchant/WebContent/js/menuManager.js
+++ O2OSystem-merchant/WebContent/js/menuManager.js
@@ -1,12 +1,23 @@
 $(function() {
 	console.log('Menu Manager');
-	$.ajax({
-		url: "http://localhost:17236/o2osystem-merchant/menuTypes",
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data.object[0]);
-			$("#menuTypeJsonData").text(data.object[0]);
+	var controller = (function(){
+		var getMenuTypes = function(){
+			$.ajax({
+				url: "http://localhost:17236/o2osystem-merchant/menuTypes",
+				type: 'GET',
+				dataType: 'json',
+				success: function(data) {
+					console.log(data.object[0]);
+					$("#menuTypeJsonData").text(data.object[0]);
+				}
+			});
+		};
+		var getMenuByType=function(){
+
+		};
+		
+		return{
+			getMenuTypes:getMenuTypes
 		}
-	});
+	})();
 });
