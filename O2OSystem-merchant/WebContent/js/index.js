8dd93c047609c3104362e04ed7fb1517f308c239
LILE8 <380034641@qq.com>
2015-08-28 08:35:35 +0800

    no message

--- O2OSystem-merchant/WebContent/js/index.js
+++ O2OSystem-merchant/WebContent/js/index.js
@@ -1,16 +1,12 @@
 $(function() {
-	// 页面默认选择newOrder.html
+	//页面默认选择newOrder.html
 	$('#change').load('html/newOrder.html');
-	// 点击切换
+	//点击切换
 	$('.list-group li').on('click', function() {
 		var loadhtml = $(this).attr('id');
 		if (loadhtml) {
 			$('#change').load('html/' + loadhtml + '.html');
+			
 		}
-
 	});
-	$("#accountInfo").live("click", function() {
-		showAccount();
-	});
-
 });
