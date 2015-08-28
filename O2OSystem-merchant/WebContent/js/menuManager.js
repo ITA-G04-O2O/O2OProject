32031692d4380347548526187794924e21ab13cb
HuangVince <584253328@qq.com>
2015-08-28 10:19:35 +0800

    no message

--- O2OSystem-merchant/WebContent/js/menuManager.js
+++ O2OSystem-merchant/WebContent/js/menuManager.js
@@ -1,20 +1,43 @@
 $(function () {
-    console.log('Menu Manager');
     var controller = (function () {
         var getMenuTypes = function (id) {
             $.ajax({
                 url: "http://localhost:17236/o2osystem-merchant/menuTypes" + id,
                 type: 'GET',
                 dataType: 'json'
-            }).done(function (data) {
-
-            }).fail(function (data) {
-
+            }).done(function (data, status, xhr) {
+                if (data.result) renderMenuType(data.object);
+            }).fail(function (xhr, status, error) {
+                console.log('fail');
+            });
+        };
+        var getMenuItems = function (id) {
+            $.ajax({
+                url: "http://localhost:17236/o2osystem-merchant/menuTypes" + id,
+                type: 'GET',
+                dataType: 'json'
+            }).done(function (data, status, xhr) {
+                if (data.result) renderMenuType(data.object);
+            }).fail(function (xhr, status, error) {
+                console.log('fail');
             });
         };
         var renderMenuType = function (data) {
-            $("#menuTypeJsonData").text(data.object[0]);
-        }
+            for (var i = 0; i < data.length; i++) {
+                $("#menuType").append('<label class="label-info">' + data[i] + '</label>');
+                getMenuItems(id);
+            }
+        };
+        var renderMenuItems = function (data) {
+            for (var i = 0; i < data.length; i++) {
+                $('#change').append('<div class="panel panel-primary"><div class="panel-heading"><div class="row"><div class="col-xs-2"><span>' + +'</span></div><div class="col-xs-10">'
+                    +'<Button class="btn btn-primary pull-right">Add</Button>'
+                    +'</div></div></div><div class="panel-body" id="menuType" >' +
+                    '<>' +
+                    '' +
+                    '</div></div>')
+            }
+        };
         var getMenuByType = function () {
 
         };
